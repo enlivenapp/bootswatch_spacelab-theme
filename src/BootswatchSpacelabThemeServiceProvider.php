c5cf8170c34f452948603bac1f266fae121a1687
Mike <sales@enlivenapp.com>
2015-03-09 23:27:34 -0400

    updates

---
+++ src/BootswatchSpacelabThemeServiceProvider.php
@@ -0,0 +1,22 @@
+<?php namespace Enlivenapp\BootswatchSpacelabTheme;
+use Illuminate\Support\ServiceProvider;
+/**
+ * Class ExampleThemeServiceProvider
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\ExampleTheme
+ */
+class BootswatchSpacelabThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register('Enlivenapp\BootswatchSpacelabTheme\BootswatchSpacelabThemeRouteProvider');
+    }
+}
